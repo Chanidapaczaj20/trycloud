6e3f8ea7bcbb4f34da7997b076cdd839424cbaf1
Chanidapaczaj20 <70244412+Chanidapaczaj20@users.noreply.github.com>
2021-02-02 00:06:01 -0500

    Added user stories

---
+++ src/test/java/com/trycloud/tests/userstories/UserStory1.java
@@ -0,0 +1,22 @@
+package com.trycloud.tests.userstories;
+
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class UserStory1 extends TestBase {
+
+
+    // Test case #2 - verify user cannot login with invalid credentials
+    //1. Navigate to login page http://qa.trycloud.net/index.php/login?clear=1
+    // 2. Enter valid username invalid password
+    //3. Click login button
+    //4. Message “Invalid user name or password.” should be displayed
+    //5. Page title and url should be same
+
+    @Test
+    public void TestCase2() {
+    }
+
+
+
+}
---
+++ src/test/java/com/trycloud/tests/userstories/UserStory2.java
@@ -0,0 +1,16 @@
+package com.trycloud.tests.userstories;
+
+import org.testng.annotations.Test;
+
+public class UserStory2 {
+
+
+
+    @Test
+    public void TestCase1() {
+    }
+
+    @Test
+    public void TestCase2() {
+    }
+}
